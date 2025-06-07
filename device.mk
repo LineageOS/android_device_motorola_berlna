cd25a53ed6f218aa5dd6aaa82420af7c223503ef
Bruno Martins <bgcngm@gmail.com>
2025-06-07 18:38:16 +0100

    berlna: Switch to Touch AIDL
    Change-Id: Id4fcd7ca97b04f887dec6939419815722097d10c

--- device.mk
+++ device.mk
@@ -90,5 +90,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
+# Touch HAL
+PRODUCT_PACKAGES += \
+    vendor.lineage.touch-service.motorola
+
+$(call soong_config_set, MOTOROLA_TOUCH, HIGH_TOUCH_POLLING_PATH, /sys/class/touchscreen/primary/interpolation)
+
 # Inherit from vendor blobs
 $(call inherit-product, vendor/motorola/berlna/berlna-vendor.mk)
